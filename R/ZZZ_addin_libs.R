7829f9544e303515031d23fe6b480e7493199926
David Waddell <git.ligament012@simplelogin.com>
2024-09-28 16:50:37 +1200

    Add lintr and full ZZZ_addin_libs

--- R/ZZZ_addin_libs.R
+++ R/ZZZ_addin_libs.R
@@ -1,7 +1,40 @@
 # renv::install(
 #   "tidyverse",
 #   "devtools",
-# "languageserver"
+#   "sinew",
+#   "styler",
+#   "devtools",
+#   "janitor",
+#   "git2r",
+#   "microbenchmark",
+#   "openxlsx",
+#   "zip",
+#   "skimr",
+#   "drat",
+#   "golem",
+#   "esquisse",
+#   "checkhelper",
+#   "languageserver"
 # )
 
-# devtools::install_github("lionel-/codegrip")
+# renv::hydrate(
+#   "codegrip",
+#   "mischelper",
+#   "viewxl",
+#   "jadd",
+#   "imageclipr",
+#   "docthis",
+#   "hippie",
+#   "datapasta"
+# )
+
+# # devtools::install_github("lionel-/codegrip")
+# # devtools::install_github("crew102/hippie")
+# # devtools::install_github("dracodoc/mischelper")
+# # devtools::install_github("dreamRs/viewxl")
+# # devtools::install_github("jennybc/jadd")
+# # devtools::install_github("Timag/imageclipr")
+# # devtools::install_github("mdlincoln/docthis")
+# # install.packages(
+# #    "datapasta", 
+# #    repos = c(mm = "https://milesmcbain.r-universe.dev", getOption("repos")))
